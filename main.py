312afa70ec27c3ae11747978473f570ee447488a
Dragomir Alin <alin.dragomir@cloudify.ro>
2021-05-19 19:29:33 +0300

    add initial script

---
+++ main.py
@@ -0,0 +1,30 @@
+import paho.mqtt.client as mqtt
+import logging
+
+
+class Publish(object):
+    def __init__(self, host, port):
+        self.connect = False
+        self.host = host
+        self.port = port
+        self.client = None
+        self.log = logging.getLogger(__name__)
+
+    def on_message(self, client, userdata, msg):
+        self.log.debug("MQTT: Publish message")
+
+    def mqtt(self):
+        try:
+            self.client = mqtt.Client()
+            status_connection = self.client.connect(self.aws_host, self.aws_port, keepalive=60)
+            if status_connection == 0:
+                self.log.info("Successfully connected to Broker!")
+                self.connect = True
+                return self
+        except ConnectionRefusedError:
+            self.log.error("Connection failed")
+        except:
+            self.log.error("Unknown error while connecting to Broker.")
+
+    def start(self):
+        self.client.loop_forever()
