39bb943478ed34b9371c0a265537234306354c34
Dragomir Alin <alin.dragomir@cloudify.ro>
2021-05-19 20:11:30 +0300

    update script

--- main.py
+++ main.py
@@ -1,30 +1,22 @@
-import paho.mqtt.client as mqtt
-import logging
+import time
+import MQTT
+
+host = "localhost"
+port = 1883
 
 
-class Publish(object):
-    def __init__(self, host, port):
-        self.connect = False
-        self.host = host
-        self.port = port
-        self.client = None
-        self.log = logging.getLogger(__name__)
+def data_DTH22(host, port, topic, frequency):
+    mqtt = MQTT.MQTT(host, port).connect()
 
-    def on_message(self, client, userdata, msg):
-        self.log.debug("MQTT: Publish message")
+    while True:
+        print("MQTT: publish on topic " + topic)
+        mqtt.publish(topic, "temperature")
+        time.sleep(frequency)
 
-    def mqtt(self):
-        try:
-            self.client = mqtt.Client()
-            status_connection = self.client.connect(self.host, self.port, keepalive=60)
-            if status_connection == 0:
-                self.log.info("Successfully connected to Broker!")
-                self.connect = True
-                return self
-        except ConnectionRefusedError:
-            self.log.error("Connection failed")
-        except:
-            self.log.error("Unknown error while connecting to Broker.")
 
-    def start(self):
-        self.client.loop_forever()
+def main():
+    data_DTH22(host, port, "city/street", 1)
+
+
+if __name__ == "__main__":
+    main()
