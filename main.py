6aec738f221d9f4d2fa3fd5a14dc162b07f5d4ce
dragomiralin <dragomirdanielalin@Yahoo.com>
2021-05-19 21:18:44 +0300

    add generator dth22

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import time
 import MQTT
+import random
+import json
+import datetime
 
 host = "localhost"
 port = 1883
@@ -10,7 +13,25 @@ def data_dth22(host, port, topic, frequency):
 
     while True:
         print("MQTT: publish on topic " + topic)
-        mqtt.publish(topic, "temperature")
+        data = {
+            "payload": {
+                "temperature": random.randint(0, 35),
+                "humidity": random.randint(0, 100)
+            },
+            "metadata": {
+                "macAddress": "02:00:00:%02x:%02x:%02x" % (random.randint(0, 255),
+                                                           random.randint(0, 255),
+                                                           random.randint(0, 255)),
+                "device": "ESP8266",
+                "location": "City, Street, nr / coordinates",
+                "timestamp": datetime.datetime.now(),
+                "maintainer": "Mr. X",
+                "lastMaintain": datetime.datetime.now()
+            }
+
+        }
+
+        mqtt.publish(topic, json.dumps(data, indent=4, sort_keys=True, default=str))
         time.sleep(frequency)
 
 
